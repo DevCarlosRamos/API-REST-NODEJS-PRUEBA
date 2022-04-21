755c2e45357925d82304261abf237de70e441294
DevCarlosRamos <74577681+DevCarlosRamos@users.noreply.github.com>
2022-04-20 19:22:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = process.env["PORT"] || 3000;
-export const MONGODB_URI = process.env["MONGODB_URI"] || "mongodb+srv://carlosdanielramosherrera:o6xEfslHvxDv9lxh@cluster-api-rest-nodejs.mtq19.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env["MONGODB_URI"];
